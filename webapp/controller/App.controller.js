57cf202159925e794f8b9d7df11245b968d64e40
termanico <klingbeil.thorsten@t-online.de>
2021-05-03 07:59:46 +0200

    Update

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,15 +1,70 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+	"sap/ui/core/mvc/Controller",
+	"sap/base/Log",
+	"../model/formatter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+	// "sap/m/MessageToast"
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
-	function (Controller) {
+	function (Controller, Log, formatter, Filter, FilterOperator) { //, MessageToast) {
 		"use strict";
 
-		return Controller.extend("opensap.movies.controller.App", {
-			onInit: function () {
+		function onInit() {
+			Log.info("Controller has been initialized.");
+		}
 
-			}
+		function onBeforeRendering() {
+			Log.info("The view will shortly be rendered.");
+		}
+
+		function onAfterRendering() {
+			Log.info("The view has been rendered.");
+		}
+
+		function onExit() {
+			Log.info("Controller will shortly be destroyed.");
+		}
+
+		function onPress(sValue) {
+			// alert(huhu);
+			// MessageToast.show("Searching...");
+			sap.ui.require(["sap/m/MessageToast"], function (oMessage) {
+				var oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+				oMessage.show(oResourceBundle.getText("search") + sValue);
+			}.bind(this));
+
+			var sCity = this.byId('city').getValue(),
+				sGenre = this.byId('genre').getSelectedItem().getKey(),
+				oCalendar = this.byId("calendar"),
+				oRowBinding = oCalendar.getBinding("rows"),
+				oFilterGenre,
+				oFilterCity;
+
+			// Create filters for genre and city according to user inputs
+			oFilterGenre = sGenre ? new Filter("genre", FilterOperator.EQ, sGenre) : null;
+			oFilterCity = sCity ? new Filter("info", FilterOperator.Contains, sCity) : null;
+
+			// Apply genre filter to calendar rows
+			oRowBinding.filter(oFilterGenre);
+
+			// Apply city filter to row appointments
+			var aRows = oCalendar.getAggregation("rows");
+			aRows.forEach(function (oItem) {
+				var oAppointmentsBinding = oItem.getBinding("appointments");
+				oAppointmentsBinding.filter(oFilterCity);
+			});
+
+		}
+
+		return Controller.extend("opensap.movies.controller.App", {
+			formatter: formatter,
+			onInit: onInit,
+			onBeforeRendering: onBeforeRendering,
+			onAfterRendering: onAfterRendering,
+			onExit: onExit,
+			onPress: onPress
 		});
 	});
